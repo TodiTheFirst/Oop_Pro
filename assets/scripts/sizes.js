676e73ac97094ff212559da1bd4b0c29d22a2420
TodiTheFirst <146870436+TodiTheFirst@users.noreply.github.com>
2024-01-12 17:21:37 +0300

    Create sizes.js

---
+++ assets/scripts/sizes.js
@@ -0,0 +1,10 @@
+// $0.getBoundingClientRect() позволяет получить доступ к css файла
+// страница парсица с левого верхнего угла 
+//$0.offsetTop позволяет узнать высоту
+//$0.offsetLeft позволяет узнать длинну
+//$0.clientTop
+//$0.clientLeft
+//$0.offsetWidth
+//$0.offsetHeight
+//$0.scrollHeight  позволяет узнать длинну контента внутри бокса в данном случае
+//всё можно посмотреть на htmlArticl 
